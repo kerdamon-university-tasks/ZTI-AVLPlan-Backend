290b74bbfe9eecabf897c78e0044ae50ff3da1d8
Konrad Walas <kwalas314@gmail.com>
2022-09-14 19:48:20 +0200

    Change authentication route

--- src/main/java/com/zti/avlplan/Authentication/Security/SecurityConfig.java
+++ src/main/java/com/zti/avlplan/Authentication/Security/SecurityConfig.java
@@ -30,13 +30,15 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+        CustomAuthenticationFilter customAuthenticationFilter = new CustomAuthenticationFilter(authenticationManagerBean());
+        customAuthenticationFilter.setFilterProcessesUrl("/api/v1/login");
         http.csrf().disable();
         http.sessionManagement().sessionCreationPolicy(STATELESS);
-        http.authorizeRequests().antMatchers("/login").permitAll();
-        http.authorizeRequests().antMatchers(GET, "api/v1/**").hasAnyAuthority("ROLE_USER");
-        http.authorizeRequests().antMatchers(POST, "api/v1/**").hasAnyAuthority("ROLE_USER");
+        http.authorizeRequests().antMatchers("/api/v1/login/**").permitAll();
+        http.authorizeRequests().antMatchers(GET, "/api/v1/avlitem/**").hasAnyAuthority("ROLE_USER");
+        http.authorizeRequests().antMatchers(POST, "/api/v1/avlitem/**").hasAnyAuthority("ROLE_USER");
         http.authorizeRequests().anyRequest().authenticated();
-        http.addFilter(new CustomAuthenticationFilter(authenticationManagerBean()));
+        http.addFilter(customAuthenticationFilter);
     }
 
     @Bean
